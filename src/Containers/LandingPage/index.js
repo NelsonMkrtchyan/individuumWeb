001ad1cbac6e858dfcb8b9fcf5c6b08daf74b4d7
nelsmkrtchyan <nelsmkrtchyan@gmail.com>
2023-02-27 20:10:34 +0400

    fix langind page imports and body

--- src/Containers/LandingPage/index.js
+++ src/Containers/LandingPage/index.js
@@ -2,24 +2,30 @@ import React from "react";
 import NavigationBar from "../../Components/NavigationBar";
 // import PriceListComponent from "../../Components/PriceListComponent";
 import Footer from "../../Components/Footer";
-// import { useTranslation } from "react-i18next";
+import SwiperComponent from "../../Components/SwiperComponent";
+import PrincipalsComponent from "../../Components/PrincipalsComponent";
+import AboutUsComponent from "../../Components/AboutUsComponent";
+import StatisticsComponent from "../../Components/StatisticsComponent";
+import DentistsComponent from "../../Components/DentistsComponent";
+import GalleryComponent from "../../Components/GalleryComponent";
+import { useTranslation } from "react-i18next";
 
 const LandingPage = () => {
-    // const { t } = useTranslation(["navbar"]);
+    const { t } = useTranslation(["navbar"]);
     return (
       <div>
           <NavigationBar />
-          {/*<SwiperComponent />*/}
-          {/*<PrincipalsComponent styles={{ marginTop: "5vw" }} />*/}
-          {/*<AboutUsComponent styles={{ marginTop: "5vw" }} />*/}
-          {/*<StatisticsComponent styles={{ marginTop: "10vw" }} />*/}
-          {/*<DentistsComponent />*/}
-          {/*/!*<PriceListComponent />*!/*/}
-          {/*<GalleryComponent*/}
-          {/*  title={t("gallery")}*/}
-          {/*  backgroundTitle={t("gallery")}*/}
-          {/*  styles={{ marginTop: "8vw" }}*/}
-          {/*/>*/}
+          <SwiperComponent />
+          <PrincipalsComponent styles={{ marginTop: "5vw" }} />
+          <AboutUsComponent styles={{ marginTop: "5vw" }} />
+          <StatisticsComponent styles={{ marginTop: "10vw" }} />
+          <DentistsComponent />
+          {/*<PriceListComponent />*/}
+          <GalleryComponent
+            title={t("gallery")}
+            backgroundTitle={t("gallery")}
+            styles={{ marginTop: "8vw" }}
+          />
           <Footer />
       </div>
     );
