6e4fc231d5c02a2d33ea68faf980f613f6eb0ed7
nelsmkrtchyan <nelsmkrtchyan@gmail.com>
2023-02-28 18:39:08 +0400

    added new location to google map

--- src/Components/GoogleMapComponent/index.js
+++ src/Components/GoogleMapComponent/index.js
@@ -4,25 +4,29 @@ import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 // AIzaSyCQXeUGlrNWNpJ9lAZ1_lxdMvDfkVyaXp4
 
 const GoogleMapComponent = () => {
-  return (
-    <LoadScript googleMapsApiKey="AIzaSyCQXeUGlrNWNpJ9lAZ1_lxdMvDfkVyaXp4">
-      <GoogleMap
-        mapContainerStyle={{
-          width: "60%",
-          height: "20vw",
-          borderRadius: "2vw",
-        }}
-        center={{ lat: 40.15123176305087, lng: 44.500573092268 }}
-        zoom={17}
-      >
-        <Marker
-          key="marker_1"
-          position={{ lat: 40.15123176305087, lng: 44.500573092268 }}
-        />
-        <></>
-      </GoogleMap>
-    </LoadScript>
-  );
+    return (
+      <LoadScript googleMapsApiKey="AIzaSyCQXeUGlrNWNpJ9lAZ1_lxdMvDfkVyaXp4">
+          <GoogleMap
+            mapContainerStyle={{
+                width: "60%",
+                height: "20vw",
+                borderRadius: "2vw"
+            }}
+            // center={{ lat: 40.15123176305087, lng: 44.500573092268 }}
+            center={{ lat: 40.17932294727421, lng: 44.517237757670756 }}
+            zoom={17}
+          >
+              <Marker
+                key="marker_1"
+                position={{ lat: 40.15140742273631, lng: 44.499909157443085 }}
+              />
+              <Marker
+                key="marker_1"
+                position={{ lat: 40.17932294727421, lng: 44.517237757670756 }}
+              />
+          </GoogleMap>
+      </LoadScript>
+    );
 };
 
 // eslint-disable-next-line import/no-anonymous-default-export
